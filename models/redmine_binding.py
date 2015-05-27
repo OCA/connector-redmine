a6fe71a63bedbbb413d64deb4ba9bc27288c6475
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2015-05-27 12:33:33 +0530

    Removed unnecessary import statements

--- models/redmine_binding.py
+++ models/redmine_binding.py
@@ -19,7 +19,7 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, api, _
+from openerp import models, fields
 
 class RedmineBinding(models.AbstractModel):
 
